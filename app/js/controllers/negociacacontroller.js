305b82a61e4c10bc6a175d0f1c80d2ba74b10557
alencartha <alencar_tha@hotmail.com>
2021-05-31 13:53:23 -0300

    novo model

--- app/js/controllers/negociacacontroller.js
+++ app/js/controllers/negociacacontroller.js
@@ -1,16 +1,13 @@
 class NegociacaoController {
-  constructor() {
-    this._inputData = document.querySelector('#data');
-    this._inputQuantidade = document.querySelector('#quantidade');
-    this._inputValor = document.querySelector('#valor');
-  }
-  adiciona(event) {
-    event.preventDefault();
-    const negociacao = new Negociacao(
-      new Date(this._inputData.value.replace(/-/g, ',')),
-      parseInt(this._inputQuantidade.value),
-      parseFloat(this._inputValor.value)
-    );
-    console.log(negociacao);
-  }
+    constructor() {
+        this._negociacoes = new Negociacoes();
+        this._inputData = document.querySelector('#data');
+        this._inputQuantidade = (document.querySelector('#quantidade'));
+        this._inputValor = document.querySelector('#valor');
+    }
+    adiciona(event) {
+        event.preventDefault();
+        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        this._negociacoes.adiciona(negociacao);
+    }
 }
--- app/ts/controllers/negociacacontroller.ts
+++ app/ts/controllers/negociacacontroller.ts
@@ -2,6 +2,7 @@ class NegociacaoController {
   private _inputData: HTMLInputElement;
   private _inputQuantidade: HTMLInputElement;
   private _inputValor: HTMLInputElement;
+  private _negociacoes = new Negociacoes();
 
   constructor() {
     this._inputData = <HTMLInputElement>document.querySelector('#data');
@@ -20,6 +21,6 @@ class NegociacaoController {
       parseFloat(this._inputValor.value)
     );
 
-    console.log(negociacao);
+    this._negociacoes.adiciona(negociacao);
   }
 }
