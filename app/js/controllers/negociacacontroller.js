c5d42ec0f7665530361ed2893f65a78a4bd3d452
alencartha <alencar_tha@hotmail.com>
2021-06-01 13:49:11 -0300

    validação dias úteis

--- app/js/controllers/negociacacontroller.js
+++ app/js/controllers/negociacacontroller.js
@@ -1,7 +1,7 @@
 System.register(["../views/index", "../models/index"], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var index_1, index_2, NegociacaoController;
+    var index_1, index_2, NegociacaoController, diaDaSemana;
     return {
         setters: [
             function (index_1_1) {
@@ -24,13 +24,31 @@ System.register(["../views/index", "../models/index"], function (exports_1, cont
                 }
                 adiciona(event) {
                     event.preventDefault();
-                    const negociacao = new index_2.Negociacao(new Date(this._inputData.val().replace(/-/g, ',')), parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
+                    let data = new Date(this._inputData.val().replace(/-/g, ','));
+                    if (this.ehDiaUtil(data)) {
+                        this._mensagemView.update('Somente negociações em dias úteis, por favor.');
+                        return;
+                    }
+                    const negociacao = new index_2.Negociacao(data, parseInt(this._inputQuantidade.val()), parseFloat(this._inputValor.val()));
                     this._negociacoes.adiciona(negociacao);
                     this._negociacoesView.update(this._negociacoes);
                     this._mensagemView.update('Negociação adicionada com sucesso!');
                 }
+                ehDiaUtil(data) {
+                    return (data.getDay() != diaDaSemana.Sabado ||
+                        data.getDay() != diaDaSemana.Domingo);
+                }
             };
             exports_1("NegociacaoController", NegociacaoController);
+            (function (diaDaSemana) {
+                diaDaSemana[diaDaSemana["Domingo"] = 0] = "Domingo";
+                diaDaSemana[diaDaSemana["Segunda"] = 1] = "Segunda";
+                diaDaSemana[diaDaSemana["Terca"] = 2] = "Terca";
+                diaDaSemana[diaDaSemana["Quarta"] = 3] = "Quarta";
+                diaDaSemana[diaDaSemana["Quinta"] = 4] = "Quinta";
+                diaDaSemana[diaDaSemana["Sexta"] = 5] = "Sexta";
+                diaDaSemana[diaDaSemana["Sabado"] = 6] = "Sabado";
+            })(diaDaSemana || (diaDaSemana = {}));
         }
     };
 });
--- app/ts/controllers/negociacacontroller.ts
+++ app/ts/controllers/negociacacontroller.ts
@@ -1,6 +1,5 @@
-import {NegociacoesView, MensagemView} from '../views/index'
-import {Negociacoes, Negociacao} from '../models/index'
-
+import { NegociacoesView, MensagemView } from '../views/index';
+import { Negociacoes, Negociacao } from '../models/index';
 
 export class NegociacaoController {
   private _inputData: JQuery;
@@ -12,7 +11,7 @@ export class NegociacaoController {
 
   constructor() {
     this._inputData = $('#data');
-    this._inputQuantidade = $('#quantidade')
+    this._inputQuantidade = $('#quantidade');
 
     this._inputValor = $('#valor');
     this._negociacoesView.update(this._negociacoes);
@@ -21,8 +20,15 @@ export class NegociacaoController {
   adiciona(event: Event) {
     event.preventDefault();
 
+    let data = new Date(this._inputData.val().replace(/-/g, ','));
+
+    if (this.ehDiaUtil(data)) {
+      this._mensagemView.update('Somente negociações em dias úteis, por favor.');
+      return
+    }
+
     const negociacao = new Negociacao(
-      new Date(this._inputData.val().replace(/-/g, ',')),
+      data,
       parseInt(this._inputQuantidade.val()),
       parseFloat(this._inputValor.val())
     );
@@ -31,4 +37,21 @@ export class NegociacaoController {
     this._negociacoesView.update(this._negociacoes);
     this._mensagemView.update('Negociação adicionada com sucesso!');
   }
+
+  private ehDiaUtil(data: Date) {
+    return (
+      data.getDay() != diaDaSemana.Sabado ||
+      data.getDay() != diaDaSemana.Domingo
+    );
+  }
+}
+
+enum diaDaSemana {
+  Domingo,
+  Segunda,
+  Terca,
+  Quarta,
+  Quinta,
+  Sexta,
+  Sabado,
 }
